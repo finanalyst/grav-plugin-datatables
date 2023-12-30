49ffdc3b4df1222e0885da8af3b45166c5fb3013
reisir <reisirito@gmail.com>
2023-12-30 16:02:08 +0200

    handle invalid id from shortcode parameters

--- shortcodes/DataTablesShortcode.php
+++ shortcodes/DataTablesShortcode.php
@@ -25,27 +25,32 @@ class DataTablesShortcode extends Shortcode
         ));
       }
 
+      // Get id from html and remove the id attribute
       $hasIdAttribute = preg_match('/(\<table\s[^>]*)id="(.*?)"(.*\>)/ims', $content, $matches);
       if ($hasIdAttribute == 1) {
-        $invalidId = (!$matches[2] || preg_match('/\s/', $matches[2]) == 1);
-        if ($invalidId) {
-          $content = $matches[1] . $matches[3];
-        } else {
-          $id = $matches[2];
-        }
+        $content = $matches[1] . $matches[3];
+        $id = $matches[2];
       }
 
-      if (!$id) {
-        $id = trim($parameters['id'] ?? $parameters['grav-id'] ?? '');
-        unset($parameters['id']);
-        unset($parameters['grav-id']);
-        if (!$id) $id = Utils::generateRandomString(10);
-        $pos = stripos('<table', $content);
-        $end = substr($content, $pos);
-        $content = substr_replace($content, " id=\"$id\" ", 7) . $end;
+      // Get id from parameters, overriding any existing id
+      $id = trim($parameters['id'] ?? $parameters['grav-id'] ?? '');
+      unset($parameters['id']);
+      unset($parameters['grav-id']);
+
+      // If there's no id or the id is invalid, generate a new id
+      $invalidId = (preg_match('/\s/', $id) == 1);
+      if ($invalidId || !$id) {
+        $id = Utils::generateRandomString(10);
       }
 
+      // Insert the id attribute
+      $content = preg_replace('/\<table/i', "\\0 id=\"$id\"", $content);
+
+      // JSON encode the shortcode parameters
+      // Stripping quotes from "true" and "false", effectively casting to boolean
       $options = preg_replace(['/"true"/i', '/"false"/i'], ['true', 'false'], json_encode($parameters));
+
+      // Process the template and decode the output
       $output = $this->twig->processTemplate(
         'partials/datatables.html.twig',
         [
