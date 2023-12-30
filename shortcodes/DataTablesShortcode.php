286891418ebaa9e079f9a9b5980dfefd98ee1de4
reisir <reisirito@gmail.com>
2023-12-30 15:11:31 +0200

    fix rendering issue, format php, clean up code, declare dependencies

--- shortcodes/DataTablesShortcode.php
+++ shortcodes/DataTablesShortcode.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Grav\Plugin\Shortcodes;
+
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 use Grav\Common\Utils;
 
@@ -7,57 +9,55 @@ class DataTablesShortcode extends Shortcode
 {
   public function init()
   {
-    $this->shortcode->getHandlers()->add('datatables', function(ShortcodeInterface $sc) {
+    $this->shortcode->getHandlers()->add('datatables', function (ShortcodeInterface $sc) {
       $content = $sc->getContent();
       $parameters = $sc->getParameters();
       $id = '';
-      $res = preg_match('/\<table[^>]*?\>(.*)\<\/table[^>]*\>/ims',$content);
-      // does table have a table attached?
-      if ( $res === FALSE or $res == 0) {
-        // error some where
-        return $this->twig->processTemplate('partials/datatables-error.html.twig',
-        [ 'message' => 'Shortcode content does not appear to have a valid &lt;table&gt;...&lt;/table&gt; element. Got instead:',
-          'content' => $content
-        ] );
-      } else {
-        $res = preg_match('/(\<table\s[^>]*)id="(.*?)"(.*\>)/ims',$content,$matches);
-        if ( $res == 1 ) {
-          $this->grav['debugger']->addMessage('found id');
-          if ( ! $matches[2] || preg_match( '/\s/',$matches[2]) == 1 ) {
-            // id either has spaces - illegal - or is null, so strip output
-            $content = $matches[1] . $matches[3];
-          } else {
-              $id = $matches[2];
-            }
-          }
-      }
-      if ( ! $id ) {
-        if ( isset( $params['grav-id'])) {
-            $id = trim($params['grav-id']);
-            unset($params['grav-id']);
-            if (preg_match('/\s/')) { $id = '';} // ignore an illegal id
-        }
-        // this occurs if content has <table without an id, or an illegal id has be stripped out
-        if (! $id ) $id = Utils::generateRandomString(10);
-        $pos = stripos('<table', $content);
-        $end = substr($content,7);
-        $content = substr_replace($content," id=\"$id\" ",7) . $end;
-      }
-      $options='';
-      if ( $parameters ) {
-        $got = array('"true"','"TRUE"','"True"','"false"','"FALSE"', '"False"' );
-        $want = array('true','true','true','false','false','false');
-        $options = str_replace($got,$want,json_encode($parameters));
-      }
-      $output = $this->twig->processTemplate('partials/datatables.html.twig',
+
+      $hasTable = preg_match('/\<table[^>]*?\>(.*)\<\/table[^>]*\>/ims', $content);
+      if ($hasTable === FALSE or $hasTable == 0) {
+        return htmlspecialchars_decode($this->twig->processTemplate(
+          'partials/datatables-error.html.twig',
           [
-            'id' => $id,
-            'content' => $content,
-            'options' => $options,
-            'snippet' => $this->grav['datatables']
-          ]);
-        $this->grav['datatables'] = ''; // clear snippet for next table invocation
-        return $output;
+            'message' => 'Shortcode content does not appear to have a valid &lt;table&gt;...&lt;/table&gt; element. Got instead:',
+            'content' => $content
+          ]
+        ));
+      }
+
+      $hasIdAttribute = preg_match('/(\<table\s[^>]*)id="(.*?)"(.*\>)/ims', $content, $matches);
+      if ($hasIdAttribute == 1) {
+        $invalidId = (!$matches[2] || preg_match('/\s/', $matches[2]) == 1);
+        if ($invalidId) {
+          $content = $matches[1] . $matches[3];
+        } else {
+          $id = $matches[2];
+        }
+      }
+
+      if (!$id) {
+        $id = trim($parameters['id'] ?? $parameters['grav-id'] ?? '');
+        unset($parameters['id']);
+        unset($parameters['grav-id']);
+        if (!$id) $id = Utils::generateRandomString(10);
+        $pos = stripos('<table', $content);
+        $end = substr($content, $pos);
+        $content = substr_replace($content, " id=\"$id\" ", 7) . $end;
+      }
+
+      $options = preg_replace(['/"true"/i', '/"false"/i'], ['true', 'false'], json_encode($parameters));
+      $output = $this->twig->processTemplate(
+        'partials/datatables.html.twig',
+        [
+          'id' => $id,
+          'content' => $content,
+          'options' => $options,
+          'snippet' => $this->grav['datatables']
+        ]
+      );
+      $output = htmlspecialchars_decode($output);
+      $this->grav['datatables'] = '';
+      return $output;
     });
   }
 }
